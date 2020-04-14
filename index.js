5d3aa1b13d1f286fab353f98509fbb2e82c4f93a
Joshua Bradley <joshuabradley012@gmail.com>
2020-04-13 18:39:14 -0700

    Updated API

--- index.js
+++ index.js
@@ -12,6 +12,80 @@ const earliestRecord = `
     FROM records
 `
 
+const rentRollQuery = (dateString) => {
+  const [year, month] = dateParts(dateString);
+  return `
+  SELECT SUM(records.amount) AS rentRoll
+    FROM records
+   WHERE MONTH(records.date) = ${month}
+     AND YEAR(records.date) = ${year}
+     AND records.type = 'Rent'
+`}
+
+const rentCollectedQuery = (dateString) => {
+  const [year, month] = dateParts(dateString);
+  return `
+  SELECT SUM(entries.amount) AS rentCollected
+    FROM entries
+   WHERE MONTH(entries.date) = ${month}
+     AND YEAR(entries.date) = ${year}
+     AND entries.type = 'Rent'
+`}
+
+const lateFeesQuery = (dateString) => {
+  const [year, month] = dateParts(dateString);
+  return `
+  SELECT SUM(entries.amount) AS lateFees
+    FROM entries
+   WHERE MONTH(entries.date) = ${month}
+     AND YEAR(entries.date) = ${year}
+     AND entries.type = 'Late Fee'
+`}
+
+const reimbursementsQuery = (dateString) => {
+  const [year, month] = dateParts(dateString);
+  return `
+  SELECT SUM(entries.amount) AS reimbursements
+    FROM entries
+   WHERE MONTH(entries.date) = ${month}
+     AND YEAR(entries.date) = ${year}
+     AND entries.type = 'Reimbursement'
+`}
+
+const paymentTimelineQuery = (dateString) => {
+  const [year, month] = dateParts(dateString);
+  return `
+  SELECT entries.date AS name,
+         SUM(entries.amount) AS collected,
+         (SELECT SUM(records.amount) AS rentRoll
+            FROM records
+           WHERE MONTH(records.date) = ${month}
+             AND YEAR(records.date) = ${year}
+             AND records.type = 'Rent'
+         ) AS due
+    FROM entries
+   WHERE MONTH(entries.date) = ${month}
+     AND YEAR(entries.date) = ${year}
+     AND entries.type = 'Rent'
+   GROUP
+      BY entries.date
+   ORDER
+      BY entries.date
+`}
+
+const paymentSourcesQuery = (dateString) => {
+  const [year, month] = dateParts(dateString);
+  return `
+  SELECT entries.source AS name,
+         SUM(entries.amount) AS value
+    FROM entries
+   WHERE MONTH(entries.date) = ${month}
+     AND YEAR(entries.date) = ${year}
+     AND entries.type = 'Rent'
+   GROUP
+      BY entries.source
+`}
+
 const paymentProgressQuery = (dateString) => {
   const [year, month] = dateParts(dateString);
   return `
@@ -47,8 +121,7 @@ const paymentProgressQuery = (dateString) => {
      AND entries.type = 'Rent'
    GROUP
       BY properties.id
-  `
-}
+`}
 
 const outstandingRentQuery = (dateString) => {
   const [year, month] = dateParts(dateString);
@@ -82,25 +155,43 @@ const outstandingRentQuery = (dateString) => {
      AND entries.type = 'Rent'
    GROUP
       BY tenants.id
-  `
-}
+`}
 
 exports.handler = async (event, context) => {
-  let rows = [];
+  let rows = {};
   const database = new Database(config);
   if (event.queryStringParameters) {
     if (event.queryStringParameters.table) {
       rows = await database.query('SELECT * FROM ??', [event.queryStringParameters.table]);
     }
-    if (event.queryStringParameters.get) {
-      if (event.queryStringParameters.get === 'earliest-record') {
-        rows = await database.query(earliestRecord);
-      }
-      if (event.queryStringParameters.get === 'payment-progress') {
-        rows = await database.query(paymentProgressQuery(event.queryStringParameters.date));
-      }
-      if (event.queryStringParameters.get === 'outstanding-rent') {
-        rows = await database.query(outstandingRentQuery(event.queryStringParameters.date));
+    if (event.queryStringParameters.fields) {
+      const date = event.queryStringParameters.date;
+      const fields = event.queryStringParameters.fields.split(',');
+      for (const field of fields) {
+        if (field === 'earliest-record') {
+          const result = await database.query(earliestRecord);
+          rows.earliestRecord = result[0].earliestRecord;
+        }
+        if (field === 'rent-roll') {
+          const result = await database.query(rentRollQuery(date));
+          rows.rentRoll = result[0].rentRoll;
+        }
+        if (field === 'rent-collected') {
+          const result = await database.query(rentCollectedQuery(date));
+          rows.rentCollected = result[0].rentCollected;
+        }
+        if (field === 'late-fees') {
+          const result = await database.query(lateFeesQuery(date));
+          rows.lateFees = result[0].lateFees;
+        }
+        if (field === 'reimbursements') {
+          const result = await database.query(reimbursementsQuery(date));
+          rows.reimbursements = result[0].reimbursements;
+        }
+        if (field === 'payment-timeline') rows.paymentTimeline = await database.query(paymentTimelineQuery(date));
+        if (field === 'payment-sources')  rows.paymentSources  = await database.query(paymentSourcesQuery(date));
+        if (field === 'payment-progress') rows.paymentProgress = await database.query(paymentProgressQuery(date));
+        if (field === 'outstanding-rent') rows.outstandingRent = await database.query(outstandingRentQuery(date));
       }
     }
   }
